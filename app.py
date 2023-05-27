e225fbff438fa72b31b0bd450ae9d42c8f2fab67
hessdale <hessdale96@gmail.com>
2023-05-27 15:36:40 -0600

    messing around with exceptions

---
+++ app.py
@@ -0,0 +1,26 @@
+a = 12
+b = 'srting'
+try:
+    a+b
+except TypeError:
+    print('wrong operand!')
+
+
+def float_convert(user_num):
+    int(user_num)
+    return(user_num)
+    
+def add_two(num_one,num_two):
+   
+    sum = num_one + num_two
+    print(sum)
+
+
+fruits=[]
+fruits.insert(1,'banana')
+fruits.insert(1,'kiwi')
+fruits.insert(1,'strawberry')
+
+print(fruits)
+
+
